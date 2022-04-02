a176d8d8081575ff44989e2d6ad1bc5bda7261e2
incognitofox <wwill2011@gmail.com>
2022-04-02 13:58:11 -0500

    heroku setup

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import audd_test
 
 app = Flask(__name__)
 
@@ -8,13 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', text='')
 
-@app.route('/rr_check', methods=["POST"])
-def rr_check():
-    url = request.form['url']
-    is_rr = audd_test.url_to_audio(url)
-    if is_rr:
-        return render_template('index.html', text='You would have been Rick Rolled. It\'s OK. We saved you. You\'re welcome.')
-    else:
-        return render_template('index.html', text='You lucked out this time.')
 if __name__ == '__main__':
      app.run(port=8080)
