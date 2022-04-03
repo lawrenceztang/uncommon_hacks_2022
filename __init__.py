0ae19d2642c2c3b1d6dbc5754b0ec5ba429fa671
Lawrence <lawrenceztang@gmail.com>
2022-04-03 12:45:27 -0400

    Flask fix

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-from app import routes
-
-if __name__ == '__main__':
-     app.run(port=8080)
