d99eaf9b6342e59e82c461510628da08aaa77c4a
Oliver <olivercai@uchicago.edu>
2022-04-03 14:15:28 -0500

    minor changes

--- app.py
+++ app.py
@@ -19,44 +19,6 @@ outputFrame = None
 lock = threading.Lock()
 # vs = VideoStream(src=0).start()
 
-def generate():
-	# grab global references to the output frame and lock variables
-	global outputFrame, lock
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
-
-def detect_motion(frameCount=32):
-	# grab global references to the video stream, output frame, and
-	# lock variables
-	global vs, outputFrame, lock
-	# initialize the motion detector and the total number of frames
-	# read thus far
-	total = 0
-    # loop over frames from the video stream
-	while True:
-		# read the next frame from the video stream, resize it,
-		# convert the frame to grayscale, and blur it
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
-		timestamp = datetime.datetime.now()
-		with lock:
-			outputFrame = frame.copy()
-
-
 from database import init_db
 
 init_db()
@@ -126,23 +88,6 @@ def queue():
     else:
         flash('Already in queue')
     return redirect(url_for('index'))
-
-
-
-
-@app.route("/video_feed")
-def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
-
-@app.route("/viewer")
-def viewer():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return render_template("viewer.html")
-
 
 # socketio = SocketIO(app)
     # import sched, time
