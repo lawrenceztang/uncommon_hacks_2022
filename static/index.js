5f884cddba08c353a22ab2037586e0cb5b798c26
Oliver <olivercai@uchicago.edu>
2022-04-02 21:13:05 -0500

    Added websocket call from server

--- static/index.js
+++ static/index.js
@@ -1,13 +1,20 @@
-const socket = new WebSocket('ws://localhost:8080');
+// const socket = new WebSocket('ws://localhost:8080');
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
 stream_url = "youtube.com"
 
 // Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-});
+// socket.addEventListener('open', function (event) {
+//     socket.send('Hello Server!');
+// });
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    var b = document.querySelector("iframe");
-    b.setAttribute("src", event.data);
-});
+// // Listen for messages
+// socket.addEventListener('message', function (event) {
+//     var b = document.querySelector("iframe");
+//     b.setAttribute("src", event.data);
+// });
+
+socket.on('change link', function(event)){
+    // var b = document.querySelector("iframe");
+    // b.setAttribute("src", event.data);
+    console.log("1 minute passed")
+}
